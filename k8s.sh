1d3fd1809305a73c071306c9e8b6a8f8941f714b
Ravivarman16 <129171351+Ravivarman16@users.noreply.github.com>
2023-09-21 14:58:52 +0530

    script to install k8s

---
+++ k8s.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#updating the os:
+apt-get update
+
+#installing java:
+apt-get install openjdk-11-jre
+
+#updating the os:
+apt-get update
+
+#installing docker:
+apt-get install -y docker.io
+
+#enable docker service:
+systemctl enable docker
+
+#starting the docker service:
+systemctl start docker
+
+#installing the kubernetes on it:
+apt-get update
+
+apt-get install -y curl apt-transport-https ca-certificates software-properties-common
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo add-apt-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+sudo swapoff -a
+
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+systemctl enable kubelet.service
+systemctl start kubelet
