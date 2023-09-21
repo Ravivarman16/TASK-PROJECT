f18a2e10a3ae276379d256ecaacb032317f3fd60
Ravivarman16 <129171351+Ravivarman16@users.noreply.github.com>
2023-09-21 14:54:17 +0530

    script to install terraform

---
+++ terraform.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#updating the os
+apt-get update
+
+#commands to install terraform 
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+apt-get update
+apt-get install -y terraform
+
+#checking the terraform version
+terraform --version
